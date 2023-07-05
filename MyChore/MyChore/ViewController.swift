13dc363949c5dc73de2e7d8454ab3b8b0f80d903
Hyemin Heo <heohm128@gmail.com>
2023-07-05 21:06:17 +0900

    [Add] project init

---
+++ MyChore/MyChore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChore
+//
+//  Created by sole on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
