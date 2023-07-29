4376cada9adc6e5c9f64f1687245faf06299cdee
yoogangee <cathyou1227@gmail.com>
2023-07-29 15:31:47 +0900

    #2 feat: 아이콘, 라벨 추가, datepicker 추가

--- MyChore/View/ChoreAdd/ChoreAddMainViewController.swift
+++ MyChore/View/ChoreAdd/ChoreAddMainViewController.swift
@@ -108,6 +108,111 @@ class ChoreAddMainViewController: UIViewController {
         return secondLine
     }()
     
+    lazy var furnitureIconView: UIImageView = {
+        let furnitureIconView = UIImageView()
+        furnitureIconView.image = UIImage(named: "icon_furniture")
+        return furnitureIconView
+    }()
+    
+    lazy var furnitureLabel: UILabel = {
+        let furnitureLabel = UILabel()
+        furnitureLabel.text = "가구"
+        furnitureLabel.font = UIFont.systemFont(ofSize: 20)
+        return furnitureLabel
+    }()
+    
+    lazy var thirdLine: UIView = {
+        let thirdLine = UIView()
+        thirdLine.backgroundColor = UIColor.mcGrey100
+        return thirdLine
+    }()
+    
+    lazy var dateIconView: UIImageView = {
+        let dateIconView = UIImageView()
+        dateIconView.image = UIImage(named: "icon_date")
+        return dateIconView
+    }()
+    
+    lazy var dateLabel: UILabel = {
+        let dateLabel = UILabel()
+        dateLabel.text = "날짜"
+        dateLabel.font = UIFont.systemFont(ofSize: 20)
+        return dateLabel
+    }()
+    
+    lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.locale = Locale(identifier: "ko-KR")
+        datePicker.datePickerMode = .date
+        //datePicker.Color = UIColor.mcSubGreen100
+        datePicker.tintColor = UIColor.mcMainGreen
+        return datePicker
+    }()
+    
+    lazy var fourthLine: UIView = {
+        let fourthLine = UIView()
+        fourthLine.backgroundColor = UIColor.mcGrey100
+        return fourthLine
+    }()
+    
+    lazy var alarmIconView: UIImageView = {
+        let alarmIconView = UIImageView()
+        alarmIconView.image = UIImage(named: "icon_alarm")
+        return alarmIconView
+    }()
+    
+    lazy var alarmLabel: UILabel = {
+        let alarmLabel = UILabel()
+        alarmLabel.text = "알림"
+        alarmLabel.font = UIFont.systemFont(ofSize: 20)
+        return alarmLabel
+    }()
+    
+    lazy var fifthLine: UIView = {
+        let fifthLine = UIView()
+        fifthLine.backgroundColor = UIColor.mcGrey100
+        return fifthLine
+    }()
+    
+    lazy var repeatIconView: UIImageView = {
+        let repeatIconView = UIImageView()
+        repeatIconView.image = UIImage(named: "icon_repeat")
+        return repeatIconView
+    }()
+    
+    lazy var repeatLabel: UILabel = {
+        let repeatLabel = UILabel()
+        repeatLabel.text = "반복"
+        repeatLabel.font = UIFont.systemFont(ofSize: 20)
+        return repeatLabel
+    }()
+    
+    lazy var sixthLine: UIView = {
+        let sixthLine = UIView()
+        sixthLine.backgroundColor = UIColor.mcGrey100
+        return sixthLine
+    }()
+    
+    lazy var managerIconView: UIImageView = {
+        let managerIconView = UIImageView()
+        managerIconView.image = UIImage(named: "icon_manager")
+        return managerIconView
+    }()
+    
+    lazy var managerLabel: UILabel = {
+        let managerLabel = UILabel()
+        managerLabel.text = "담당자"
+        managerLabel.font = UIFont.systemFont(ofSize: 20)
+        return managerLabel
+    }()
+    
+    lazy var seventhLine: UIView = {
+        let seventhLine = UIView()
+        seventhLine.backgroundColor = UIColor.mcGrey100
+        return seventhLine
+    }()
+    
     func setUpView() {
         view.addSubview(choreNameLabel)
         view.addSubview(choreTextField)
@@ -117,6 +222,22 @@ class ChoreAddMainViewController: UIViewController {
         view.addSubview(placeLabel)
         view.addSubview(placeButton1)
         view.addSubview(secondLine)
+        view.addSubview(furnitureIconView)
+        view.addSubview(furnitureLabel)
+        view.addSubview(thirdLine)
+        view.addSubview(dateIconView)
+        view.addSubview(dateLabel)
+        view.addSubview(datePicker)
+        view.addSubview(fourthLine)
+        view.addSubview(alarmIconView)
+        view.addSubview(alarmLabel)
+        view.addSubview(fifthLine)
+        view.addSubview(repeatIconView)
+        view.addSubview(repeatLabel)
+        view.addSubview(sixthLine)
+        view.addSubview(managerIconView)
+        view.addSubview(managerLabel)
+        view.addSubview(seventhLine)
     }
     
     func setConstraints() {
@@ -159,6 +280,85 @@ class ChoreAddMainViewController: UIViewController {
         secondLine.snp.makeConstraints { make in
             make.height.equalTo(2)
             make.top.equalTo(placeButton1.snp.bottom).offset(18)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        furnitureIconView.snp.makeConstraints { make in
+            make.height.width.equalTo(32)
+            make.top.equalTo(secondLine.snp.bottom).offset(16)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+        }
+        furnitureLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(furnitureIconView.snp.centerY)
+            make.left.equalTo(furnitureIconView.snp.right).offset(8)
+        }
+        thirdLine.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.top.equalTo(furnitureIconView.snp.bottom).offset(14)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        dateIconView.snp.makeConstraints { make in
+            make.height.width.equalTo(32)
+            make.top.equalTo(thirdLine.snp.bottom).offset(16)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(dateIconView.snp.centerY)
+            make.left.equalTo(dateIconView.snp.right).offset(8)
+        }
+        datePicker.snp.makeConstraints { make in
+            make.centerY.equalTo(dateIconView.snp.centerY)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        fourthLine.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.top.equalTo(dateIconView.snp.bottom).offset(14)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        alarmIconView.snp.makeConstraints { make in
+            make.height.width.equalTo(32)
+            make.top.equalTo(fourthLine.snp.bottom).offset(16)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+        }
+        alarmLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(alarmIconView.snp.centerY)
+            make.left.equalTo(alarmIconView.snp.right).offset(8)
+        }
+        fifthLine.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.top.equalTo(alarmIconView.snp.bottom).offset(14)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        repeatIconView.snp.makeConstraints { make in
+            make.height.width.equalTo(32)
+            make.top.equalTo(fifthLine.snp.bottom).offset(16)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+        }
+        repeatLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(repeatIconView.snp.centerY)
+            make.left.equalTo(repeatIconView.snp.right).offset(8)
+        }
+        sixthLine.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.top.equalTo(repeatIconView.snp.bottom).offset(14)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
+            make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
+        }
+        managerIconView.snp.makeConstraints { make in
+            make.height.width.equalTo(32)
+            make.top.equalTo(sixthLine.snp.bottom).offset(16)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+        }
+        managerLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(managerIconView.snp.centerY)
+            make.left.equalTo(managerIconView.snp.right).offset(8)
+        }
+        seventhLine.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.top.equalTo(managerIconView.snp.bottom).offset(14)
             make.left.equalTo(self.view.safeAreaLayoutGuide).offset(24)
             make.right.equalTo(self.view.safeAreaLayoutGuide).offset(-24)
         }
