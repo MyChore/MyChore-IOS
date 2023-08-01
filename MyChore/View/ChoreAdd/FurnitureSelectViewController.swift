d5878362fbca03310f152036c31a5d9ba4c84d9e
yoogangee <cathyou1227@gmail.com>
2023-08-01 13:53:29 +0900

    #2 feat: 선택 페이지 파일 추가

---
+++ MyChore/View/ChoreAdd/FurnitureSelectViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FurnitureSelectViewController.swift
+//  MyChore
+//
+//  Created by 경유진 on 2023/07/31.
+//
+
+import Foundation
---
+++ MyChore/View/ChoreAdd/RepeatSelectViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RepeatSelectViewController.swift
+//  MyChore
+//
+//  Created by 경유진 on 2023/07/31.
+//
+
+import Foundation
