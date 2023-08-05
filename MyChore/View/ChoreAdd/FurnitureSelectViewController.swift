4a0acf6d92c59cf9070a4352fdf55cf35280182b
yoogangee <cathyou1227@gmail.com>
2023-08-05 17:55:20 +0900

    #2 feat: 가구 선택 페이지 UI 완성

--- MyChore/View/ChoreAdd/FurnitureSelectViewController.swift
+++ MyChore/View/ChoreAdd/FurnitureSelectViewController.swift
@@ -5,4 +5,152 @@
 //  Created by 경유진 on 2023/07/31.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+struct F_Test {
+    let furniture: String
+    
+    static var data = [
+        F_Test(furniture: "티비"),
+        F_Test(furniture: "1인용 소파"),
+        F_Test(furniture: "3인용 소파"),
+        F_Test(furniture: "책장")]
+}
+
+class FurnitureSelectViewController: UIViewController, UICollectionViewDelegate {
+    
+    let testdata = F_Test.data
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        
+        furnitureCollectionView.delegate = self
+        furnitureCollectionView.dataSource = self
+        furnitureCollectionView.register(FurnitureCollectionViewCell.self, forCellWithReuseIdentifier: FurnitureCollectionViewCell.identifier)
+        
+        initNavigation()
+        view.addSubview(furnitureCollectionView)
+        
+        furnitureCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview().offset(24)
+            make.right.equalToSuperview().offset(-24)
+        }
+    }
+    
+    private let furnitureCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    func initNavigation() {
+        let backButton = UIBarButtonItem(image: UIImage(named: "icon_back"), style: .plain, target: self, action: #selector(backMainPage))
+        backButton.tintColor = UIColor.mcGrey800
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "가구"
+        titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        
+        let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(sendData))
+        
+        
+        self.navigationItem.leftBarButtonItem = backButton
+        self.navigationItem.titleView = titleLabel
+        self.navigationItem.rightBarButtonItem = doneButton
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.mcMainGreen
+    }
+    
+    @objc func backMainPage() {
+        self.navigationController?.popViewController(animated: true)
+        print("페이지 pop") // pop 표시
+    }
+    
+    @objc func sendData() {
+        print("페이지 pop")
+    }
+}
+
+extension FurnitureSelectViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        testdata.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = furnitureCollectionView.dequeueReusableCell(withReuseIdentifier: FurnitureCollectionViewCell.identifier, for: indexPath) as?
+                    FurnitureCollectionViewCell else {
+                        return UICollectionViewCell()
+                    }
+
+        cell.furnitureLabel.text = testdata[indexPath.row].furniture
+        cell.backgroundColor = .mcGrey200
+        return cell
+    }
+}
+
+extension FurnitureSelectViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = (collectionView.frame.width / 2) - 5.0
+        return CGSize(width: width, height: 68)
+    }
+}
+
+class FurnitureCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "FurnitureCollectionViewCell"
+    
+    let furnitureLabel: UILabel = {
+        var furnitureLabel = UILabel()
+        furnitureLabel.font = UIFont.systemFont(ofSize: 18)
+        return furnitureLabel
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpCell()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpCell()
+        setConstraints()
+    }
+    
+    func setUpCell() {
+        self.addSubview(furnitureLabel)
+        layer.cornerRadius = 10
+    }
+    
+    func setConstraints() {
+        furnitureLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+    }
+}
+
+
+/*
+import SwiftUI
+
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = FurnitureSelectViewController //뷰 컨트롤러 이름
+    
+    func makeUIViewController(context: Context) -> FurnitureSelectViewController {
+            return FurnitureSelectViewController() // 뷰컨트롤러 이름
+        }
+
+        func updateUIViewController(_ uiViewController: FurnitureSelectViewController, context: Context) {
+        }
+}
+
+@available(iOS 16.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+            ViewControllerRepresentable()
+    }
+}
+*/
