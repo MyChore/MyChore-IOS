d5e74bb4b17d6c4fa018484836fb7ffd898345b6
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2023-08-06 23:36:33 +0900

    Revert "Feat/1 login UI"

--- MyChore/MyChore/AppDelegate.swift
+++ MyChore/MyChore/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        IQKeyboardManager.shared.enable = true
-        IQKeyboardManager.shared.enableAutoToolbar = false
-        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
-
         return true
     }
 
